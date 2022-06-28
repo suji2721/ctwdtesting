91c428b986d1b15612c47c67a623a8efeed0652e
sreen <sreen@192.168.1.12>
2022-06-28 16:51:38 +1000

    new code commited

--- src/main/java/com/flip/kart/FlikTest.java
+++ src/main/java/com/flip/kart/FlikTest.java
@@ -2,15 +2,7 @@ package com.flip.kart;
 
 import java.util.Set;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.Test;
+
 
 
 public class FlikTest {
